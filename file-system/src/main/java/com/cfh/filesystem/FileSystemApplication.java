efcaab5219b9beddf3ea9fe87fac58ad8de27144
chenfahui <chenfahui123@hotmail.com>
2018-12-28 14:14:59 +0800

    first

---
+++ file-system/src/main/java/com/cfh/filesystem/FileSystemApplication.java
@@ -0,0 +1,14 @@
+package com.cfh.filesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSystemApplication.class, args);
+	}
+
+}
+
