ff10982c295a110b2a37aa76105a142230d0e325
chenfahui <chenfahui123@hotmail.com>
2018-12-29 10:34:03 +0800

    初始化

---
+++ file-system/file-system-client/src/main/java/com/cfh/filesystemclient/FileSystemClientApplication.java
@@ -0,0 +1,14 @@
+package com.cfh.filesystemclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSystemClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSystemClientApplication.class, args);
+	}
+
+}
+
---
+++ file-system/file-system-core/src/main/java/com/cfh/filesystemcore/FileSystemCoreApplication.java
@@ -0,0 +1,14 @@
+package com.cfh.filesystemcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSystemCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSystemCoreApplication.class, args);
+	}
+
+}
+
